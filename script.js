707812c446a9039b725c23ca205e6d709a501861
Mimansa De <165243969+macxbeast@users.noreply.github.com>
2024-09-20 01:13:40 +0530

    Updated

--- script.js
+++ script.js
@@ -37,14 +37,14 @@ function displayWeather(data) {
     const weatherIcon = document.getElementById('weather-icon');
     const hourlyForecastDiv = document.getElementById('hourly-forecast');
 
-    // Clear previous content
     weatherInfoDiv.innerHTML = '';
     hourlyForecastDiv.innerHTML = '';
     tempDivInfo.innerHTML = '';
 
     if (data.cod === '404') {
         weatherInfoDiv.innerHTML = `<p>${data.message}</p>`;
-    } else {
+    } 
+    else {
         const cityName = data.name;
         const temperature = Math.round(data.main.temp - 273.15);
         const description = data.weather[0].description;
